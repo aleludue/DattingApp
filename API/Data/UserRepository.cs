61f3e0eba16369fb07b5f4280984ebef52f27bb1
aleludue <aleludue@hotmail.com>
2023-01-18 16:15:41 -0300

    End Section 8

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,67 @@
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public UserRepository(DataContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<MemberDto> GetMemberAsync(string username)
+        {
+            return await _context.Users
+                .Where(x => x.UserName == username)
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        {
+            return await _context.Users
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {   
+            //forma alternativa de agregar el include
+            return await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            //el include es para incluir en la consulta los datos de las tablas relacionadas
+            //se tiene que poner si o si cuando existe una tabla relacional
+            return await _context.Users.Include(p => p.Photos).ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            //se pone el >0 para obtener un resultado boolean ya que el guardado nos dice la  
+            //cantidad de registros que se guardaron en la base y, si falla, devuelve 0
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
